beb145188af9240e2af6618acd227645701531e3
kangyupeng <kangyupeng@goatai.cn>
2022-09-29 18:50:00 +0800

    优化，代码，注释，案例2

--- cnn/pythonDemo/demo3/neuralnet_mnist.py
+++ cnn/pythonDemo/demo3/neuralnet_mnist.py
@@ -45,6 +45,7 @@ def predict(network, x):
 x, t = get_data()
 print(x.shape)#(10000, 784)  有一万个图像，每个图像由784个值组成
 print(x[0].shape)#(784,)
+print(t.shape)#(10000,)
 network = init_network()
 W1,W2,W3 = network['W1'],network['W2'],network['W3']
 print(W1.shape)#(784, 50) 784行50列，行乘列，会产生50个神经元（或神经元的值），然后每一个神经元都会接收784个参数，各个神经元所接收的参数，在和神经元权重进行计算。这里已经思考清楚了，如果以后不懂，重读，别就读一块，整章整章的重读，那样效果更好
@@ -71,7 +72,9 @@ for i in range(0,len(x),batch_size):
     #range函数，高级用法range(start,end,step)，会生成一个start到end-1之间构成的整数数组，但每个数据项之间间隔step
     #如 print(list(range(0,10,3)))#[0, 3, 6, 9]
     x_batch = x[i:i+batch_size]#取i到i+batch_size之间的数据
+    #print(x_batch.shape)#(100, 784)
     y_batch = predict(network,x_batch)
+    #print(y_batch.shape)#(100, 10) #这个100不是输出层上一层神经元的数量，是x_batch输入数据的时候x_batch.shape(100, 784)这个的100
     p = np.argmax(y_batch,axis=1)#获取数据最大索引值，为什么axis=1，还是看书吧，看书就会明白
     accuracy_cnt_2 += np.sum(p == t[i:i+batch_size])
 
