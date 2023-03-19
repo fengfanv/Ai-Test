6cd13ae59a959051e43cd6f97cabdbe4a7542fa8
fengfanv <1431826670@qq.com>
2023-03-19 19:48:39 +0800

    更新关于numpy.shape的原理描述“

--- Deep-learning/jsStudyNote1/common/numpy/index.js
+++ Deep-learning/jsStudyNote1/common/numpy/index.js
@@ -122,7 +122,7 @@ function allItemSum(arr) {
 ]
 3,2,2 三个两行两列
 
-
+let arr = 
 [
     [
         [
@@ -155,5 +155,56 @@ function allItemSum(arr) {
 ]
 2,3,2,2 两个 3,2,2 
 
+获取上面arr数据的形状
+获取数组维度(形状)，分为两步，1、默认数组是符合标准的数据，然后直接获取数据的形状，2、拿着获取到的形状数据，检查数据是否是符合标准的形状
+1、获取数据的形状
+准备一个数组用来存放形状数据
+let shapeArr = [];
+第一层直接获取arr的长度，这里arr.length是2，所以将2放进形状数组
+现在的shapeArr形状数组是[2]
+
+第二层，获取arr[0].length，这里arr[0].length是3，将3放进形状数组
+现在的shapeArr形状数组是[2,3]
+
+第三层，获取arr[0][0].length，这里arr[0][0].length是2，将2放进形状数组
+现在的shapeArr形状数组是[2,3,2]
+
+第四层，获取arr[0][0][0].length，这里arr[0][0][0].length是2，将2放进形状数组
+现在的shapeArr形状数组是[2,3,2,2]
+
+2、根据获取到的数据的形状，检查数据是否符合标准
+上面获取到的形状是[2,3,2,2]
+下面利用这个数据，使用递归方法，检查数据的所有节点是否都是符合上面这个形状，如果检查过程中，如果有某个节点数据形状不对，则报错
+
+//下面是检查方法
+function shapeCheck(arr,shape,dim){
+    arr是数据
+    shape形状数组
+    dim 默认为0，表示当前检查的是shape数组的第几个，假如形状数据是[4,3,2,2]，当前检查的是第0层，也就是数组里下标为0的那个数据，也就是4
+
+    let arrLen = arr.length;
+    if(arrLen == shape[dim]){
+        //正确，则继续往里面，检查
+        if(shape.length-1 == dim){
+            //这是最后一层了，在往里面没有东西了
+            return true
+        }else{
+            //不是最后一层，里面还有东西
+            let newDim = dim++;
+            for(let i=0;i<arrLen;i++){
+                let itemArr = arr[i];
+                if(Array.isArray(itemArr)){
+                    shapeCheck(itemArr,shape,newDim)
+                }else{
+                    //不是数组，抛错
+                    throw new Error('检查过程中发现非数组类型！')
+                }
+            }
+        }
+    }else{
+        //形状不正确，抛错
+        throw new Error('检查发现，数据形状在，'+shape+'：'+dim+'，处不正确！')
+    }
+}
 
 */
