3ae1c313d168c5e58d695abf99ffd081df329218
kangyupeng <kangyupeng@goatai.cn>
2023-03-21 18:58:30 +0800

    新增numpy.create_array方法的原理编写，numpy.size方法的原理编写,numpy.arange原理的编写，numpy.reshape思维编写初始化

--- Deep-learning/jsStudyNote1/common/numpy/index.js
+++ Deep-learning/jsStudyNote1/common/numpy/index.js
@@ -218,18 +218,85 @@ function shapeCheck(arr,shape,ndim){
 
 
 /*
-下面是关于，根据形状数组，生成数据的方法(numpy.create_array)
+下面是关于，根据形状数组，生成数据的方法(numpy.create_array)，这个方法类似numpy里的（numpy.empty，numpy.zeros，numpy.ones）方法类似
 
 这个方法接受，两个参数，一个是shape数组，另一个是，初始化的数据，使用什么数据进行初始化（初始化的数据，目前仅支持，纯数字，以后会支持funtion）
 
 首先准备一个空数组，用来装待会生成的数据
 let arr = [];
 
+假如形状数组是shape = [4,3,2,1]
 
+倒着循环形状数据，从里往外构建
+
+function Create_a(shape, ndim, data) {
+    let arr = [];
+    data = typeof data == 'Number' ? data : JSON.parse(JSON.stringify(data));
+    if (ndim < 0) {
+        return data;//最后一层了，直接返回数据
+    } else {
+        //不是最后一层，继续循环构建数据
+        for (let i = 0; i < shape[ndim]; i++) {
+            arr.push(data)
+        }
+        return Create_a(shape, ndim - 1, arr)
+    }
+}
+
+function create_array(shape,value){
+    return Create_a(shape,shape.length-1,value)
+}
+
+数据形状是 [4,3,2,1]，然后你想给数据结构里填充的都是数值5，先创建形状数组最后一个的数据（从里往外构建），创建1的，这时方法的ndim是3，这时方法data是5
+
+创建完毕最里面的数据之后，创建数据形状数组里的那个2的那层，这时方法的ndim是2，方法的data是一个数组[5]
+
+然后按照上面的顺序，直到创建到数据形状数组的4的那个数据，最后返回生成的数据
+
+*/
+
+
+/*
+下面是关于，获取数组（矩阵）的元素的总个数的功能实现(numpy.size)
+
+实现方法，使用上面的，获取数据形状数组的方法，shape，得到形状数组后，将里面的值都相乘，然后得到数据的元素个数。
+
+*/
+
+/*
+下面是关于，根据数值范围创建数组(numpy.arange)的方法
+
+function arange(start,stop,step){
+    //start起始值，默认为0
+    //stop终止值（不包含）
+    //step步长，默认为1
+
+    let arr = [];
+    let value = start;
+    for(let i=start;i<stop;i++){
+        arr.push(value);
+        value=value+1;
+    }
+
+    return arr
+}
+*/
+
+
+/*
+下面是关于，修改数据形状的方法（numpy.reshape）
 
 
 
 
 */
+
+
+
+
+/*
+下面是关于，
+*/
+
 
 
