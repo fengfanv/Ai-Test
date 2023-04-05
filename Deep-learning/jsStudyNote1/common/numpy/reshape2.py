403c95df47b21b06b4d32b0c8676a6c748453ff2
fengfanv <1431826670@qq.com>
2023-04-05 16:52:47 +0800

    1、新增了一个按照order=C方式将数据拉成一维数据的方法。2、编写了一些关于实现order=F相关方法的注意事项及可行性描述

--- Deep-learning/jsStudyNote1/common/numpy/reshape2.py
+++ Deep-learning/jsStudyNote1/common/numpy/reshape2.py
@@ -1,6 +1,22 @@
 import numpy as np
 
-a = np.array([
+a1 = np.array([
+    [1, 2, 3],
+    [4, 5, 6]
+])
+
+a2 = np.array([
+    [
+        [1, 2, 3],
+        [4, 5, 6]
+    ],
+    [
+        [7, 8, 9],
+        [10, 11, 12]
+    ]
+])
+
+a3 = np.array([
     [
         [
             [1, 2, 3],
@@ -22,21 +38,94 @@ a = np.array([
         ]
     ]
 ])
-# print(a.shape)
 
-# print(np.reshape(a,(8,-1),order='C'))
-
-# print(np.reshape(a,(8,-1),order='F'))
-
-b = np.array([
+a4 = np.array([
     [
-        [1, 2, 3],
-        [4, 5, 6]
+        [
+            [
+                [1, 2, 3],
+                [4, 5, 6]
+            ],
+            [
+                [7, 8, 9],
+                [10, 11, 12]
+            ]
+        ],
+        [
+            [
+                [13, 14, 15],
+                [16, 17, 18]
+            ],
+            [
+                [19, 20, 21],
+                [22, 23, 24]
+            ]
+        ]
     ],
     [
-        [7, 8, 9],
-        [10, 11, 12]
+        [
+            [
+                [1, 2, 3],
+                [4, 5, 6]
+            ],
+            [
+                [7, 8, 9],
+                [10, 11, 12]
+            ]
+        ],
+        [
+            [
+                [13, 14, 15],
+                [16, 17, 18]
+            ],
+            [
+                [19, 20, 21],
+                [22, 23, 24]
+            ]
+        ]
     ]
 ])
 
-print(np.reshape(b,(4,-1),order='F'))
+
+# print('a1.shape', a1.shape)
+# print("np.reshape(a1, (-1), order='F')")
+# print(np.reshape(a1, (-1), order='F'))
+# print('-------------------------------------------')
+# print('a2.shape', a2.shape)
+# print("np.reshape(a2, (-1), order='F')")
+# print(np.reshape(a2, (-1), order='F'))
+# print('-------------------------------------------')
+# print('a3.shape', a3.shape)
+# print("np.reshape(a3, (-1), order='F')")
+# print(np.reshape(a3, (-1), order='F'))
+# print('-------------------------------------------')
+# print('a4.shape', a4.shape)
+# print("np.reshape(a4, (-1), order='F')")
+# print(np.reshape(a4, (-1), order='F'))
+
+# print("np.reshape(a4, (4, -1), order='F')")
+# print(np.reshape(a4, (4, -1), order='F'))
+
+# print("np.reshape(a4, (3,3, -1), order='F')")
+# print(np.reshape(a4, (3, 2, -1), order='F'))
+
+
+# print("np.reshape(a4, (8, -1), order='C')：")
+# print(np.reshape(a4, (8, -1), order='C'))
+# print("np.reshape(a4, (4, -1), order='C')")
+# print(np.reshape(a4, (4, -1), order='C'))
+# print("np.reshape(a4, (3, -1), order='C')")
+# print(np.reshape(a4, (3, -1), order='C'))
+# print("np.reshape(a4, (2, 3, -1), order='C')")
+# print(np.reshape(a4, (2, 3, -1), order='C'))
+# print("np.reshape(a4, (-1), order='C')")
+# print(np.reshape(a4, (-1), order='C'))
+# print("np.reshape(a4, (2, 2, 2, -1), order='C')")
+# print(np.reshape(a4, (2, 2, 2, -1), order='C'))
+# print("np.reshape(a4, (2, 2, 2, 2, -1), order='C')")
+# print(np.reshape(a4, (2, 2, 2, 2, -1), order='C'))
+# print("np.reshape(a4, (2, 2, 2, 2, 1, -1), order='C')")
+# print(np.reshape(a4, (2, 2, 2, 2, 1, -1), order='C'))
+
+# print(np.reshape(a4, (8, -1), order='F'))
+# print(np.reshape(a4, (-1), order='F'))
