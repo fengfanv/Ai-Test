a8328e7decc674279e96c0a9012f0b913de255cd
fengfanv <1431826670@qq.com>
2023-04-10 16:37:23 +0800

    实现python版printArr方法，找到一个关于讲解numpy.transpose的网页

--- Deep-learning/jsStudyNote1/common/numpy/reshape2.py
+++ Deep-learning/jsStudyNote1/common/numpy/reshape2.py
@@ -1,6 +1,5 @@
 import numpy as np
 
-# def printArr(arr,indexArr=[],callback):
 
 
 a1 = np.array([
--- Deep-learning/jsStudyNote1/common/numpy/transpose.py
+++ Deep-learning/jsStudyNote1/common/numpy/transpose.py
@@ -1,18 +1,52 @@
 import numpy as np
 
+def printArrCallback(res):
+    print(res)
+
+def printArr(arr=[], indexArr=[]):
+    for index in range(len(arr)):
+        item = arr[index]
+        newIndexArr = indexArr.copy()
+        newIndexArr.append(index)
+        if str(type(item)) == "<class 'numpy.ndarray'>":
+            printArr(item, newIndexArr)
+        else:
+            printArrCallback({"index": newIndexArr, "value": item})
+
+
 a = np.array([
-    [1,2,3],
-    [4,5,6]
+    [1, 2, 3],
+    [4, 5, 6],
+    [7, 8, 9]
 ])
 
-print(a.transpose(0,1))
+# print(a.transpose(0, 1))
 '''
 [[1 2 3]
- [4 5 6]]
+ [4 5 6]
+ [7 8 9]]
 '''
-print(a.transpose(1,0))
+# b = a.transpose(1, 0)
+# print(b)
 '''
-[[1 4]
- [2 5]
- [3 6]]
+[[1 4 7]
+ [2 5 8]
+ [3 6 9]]
 '''
+# printArr(a)
+# print('-------------------------')
+# printArr(b)
+
+# print('-------------------------')
+c = np.array([
+    [
+        [1,2,3],
+        [4,5,6]
+    ],
+    [
+        [1,2,3],
+        [4,5,6]
+    ]
+])
+print(c.shape)
+print(c.transpose().strides)
