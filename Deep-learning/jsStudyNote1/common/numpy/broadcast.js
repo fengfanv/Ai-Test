45c8fdb16a558e89ee1b122434b81d94b472da36
fengfanv <1431826670@qq.com>
2023-05-03 16:44:17 +0800

    numpy的广播机制开始编写

---
+++ Deep-learning/jsStudyNote1/common/numpy/broadcast.js
@@ -0,0 +1,14 @@
+const Main = require('./main.js');
+var shape = Main.shape;
+
+const Common = require('./common.js');
+var printArr = Common.printArr;
+
+/*
+
+实现numpy.broadcast方法
+
+关于numpy.broadcast方法的运行基本操作，请看“broadcast.py”细心看，一看你就能明白
+
+*/
+
