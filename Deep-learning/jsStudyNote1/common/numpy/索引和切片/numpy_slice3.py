75c3b74007bfd0122a5374cd9de3ff302d5566da
fengfanv <1431826670@qq.com>
2023-10-26 22:19:30 +0800

    新增一个索引切片案例

---
+++ Deep-learning/jsStudyNote1/common/numpy/索引和切片/numpy_slice3.py
@@ -0,0 +1,78 @@
+# a=[False,True,False,True]
+# shape (4)
+
+# a=a[:,None]
+# [
+# 	[False],
+# 	[True],
+# 	[False],
+# 	[True]
+# ]
+# shape (4,1)
+
+# ----------------
+
+# a=[
+# 	[False],
+# 	[True],
+# 	[False],
+# 	[True]
+# ]
+# shape (4,1)
+
+# b=[0,2]
+# shape (2)
+
+# --------------
+
+# a=[
+# 	[False,False],
+# 	[True,True],
+# 	[False,False],
+# 	[True,True]
+# ]
+# shape (4,2)
+
+# b=[0,2]
+# shape (2)
+
+# --------------
+
+# a=[
+# 	[False,False],
+# 	[True,True],
+# 	[False,False],
+# 	[True,True]
+# ]
+# shape (4,2)
+
+# b=[[0,2]]
+# shape (1,2)
+
+# --------------
+
+# a=[
+# 	[False,False],
+# 	[True,True],
+# 	[False,False],
+# 	[True,True]
+# ]
+# shape (4,2)
+
+# b=[
+# 	[0,2],
+# 	[0,2],
+# 	[0,2],
+# 	[0,2]
+# ]
+# shape (4,2)
+
+# --------------
+
+# a+b
+# [
+# 	[0,2],
+# 	[1,3],
+# 	[0,2],
+# 	[1,3]
+# ]
