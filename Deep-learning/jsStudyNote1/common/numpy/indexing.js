49c4f34b069ed73d4f69d20aad9500ea8998cb73
kangyupeng <kangyupeng@goatai.cn>
2023-11-27 18:09:56 +0800

    开始编写基本索引...

---
+++ Deep-learning/jsStudyNote1/common/numpy/indexing.js
@@ -0,0 +1,59 @@
+/*
+索引和切片
+*/
+
+
+
+
+
+function indexing(arr, indexingArr) {
+    /*
+    arr 被索引数据
+    indexingArr py里索引元祖，js这里用一个一维数组代替
+    */
+    /*
+    处理流程：
+    1、根据 indexingArr(索引元祖) 判断索引是基本索引还是高级索引
+    2、根据索引类型将 数据和索引 交给相关方法去处理
+    3、将索引结果返回
+    */
+
+
+}
+
+
+
+//基本索引
+//处理基本索引
+function basicIndexing(arr, indexingArr) {
+    /*
+    arr 被索引数据
+    indexingArr py里索引元祖，js这里用一个一维数组代替
+    */
+    /*
+    处理流程：
+    1、根据 arr和indexingArr 推算出，索引结果的形状。
+    2、获取索引结果里的数据(不用考虑形状)
+    3、将获取的数据 转换成 索引结果的形状
+    4、返回索引结果
+    */
+
+
+}
+
+
+//高级索引
+//处理高级索引(整数数组索引,布尔数组索引)和高级索引与基本索引相结合
+function advancedIndexing(arr, indexingArr) {
+    /*
+    arr 被索引数据
+    indexingArr py里索引元祖，js这里用一个一维数组代替
+    */
+    /*
+    处理流程：
+    1、根据 arr和indexingArr 判断是 纯高级索引 还是 高级索引与基本索引相结合
+    ...
+    ...
+    */
+
+}
