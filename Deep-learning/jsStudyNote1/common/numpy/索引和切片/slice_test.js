46090e985d0c6c905b1a6676d4afed3c81b9d031
kangyupeng <kangyupeng@goatai.cn>
2023-11-30 18:43:54 +0800

    为编写基本索引做准备，目前没有考虑到，整数索引值是负值的情况，没有考虑到slice(i,j,k) ijk是负值的情况

--- Deep-learning/jsStudyNote1/common/numpy/索引和切片/slice_test.js
+++ Deep-learning/jsStudyNote1/common/numpy/索引和切片/slice_test.js
@@ -1,21 +1,45 @@
 
-let arr = [0,1,2,3,4,5,6]
+let arr = [0, 1, 2, 3, 4, 5, 6]
 
-function slice(i,j,k,arr){
+function slice(arr, i, j, k) {
+    if(k==0){
+        throw new Error('slice:error slice(i,j,k) k不能是0')
+    }
+    let n = arr.length;
+    if (typeof k != 'number') {
+        k = 1;
+    }
+    if (typeof i != 'number') {
+        i = k>0?0:n-1;
+    }
+    if (typeof j != 'number') {
+        j = k>0?n:-n-1;
+    }
+
+    
+    
+
+
+
+
+
     let index = i;
     let newArr = [];
-    while(index>=0 && index<arr.length){
+    while (index >= 0 && index < arr.length) {
         newArr.push(arr[index])
         index = index + k;
     }
-    console.log('newArr：',newArr);
+    console.log('newArr：', newArr);
 }
 
-// slice(6,1,-1,arr)
+// slice(arr,6,1,-1)
 // newArr： [ 6, 5, 4, 3, 2, 1, 0]
 
-// slice(1,6,2,arr)
+// slice(arr,1,6,2)
 // newArr： [ 1, 3, 5 ]
 
-// slice(6,1,-2,arr)
+// slice(arr,6,1,-2)
 // newArr： [ 6, 4, 2, 0 ]
+
+// slice(arr,6,1,-3)
+// newArr： [6, 3, 0]
