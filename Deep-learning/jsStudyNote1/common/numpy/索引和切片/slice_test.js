2e3f4e186043e901a1c45b0f2dd4e2ca3a46ea73
fengfanv <1431826670@qq.com>
2023-12-03 19:16:05 +0800

    编写基本索引6

--- Deep-learning/jsStudyNote1/common/numpy/索引和切片/slice_test.js
+++ Deep-learning/jsStudyNote1/common/numpy/索引和切片/slice_test.js
@@ -1,45 +1,172 @@
 
-let arr = [0, 1, 2, 3, 4, 5, 6]
+function slice(d, i, j, k) {
+    if (k == 0) return console.log(`k不能是0`);
+    console.log(`before d=${d} i=${i} j=${j} k=${k}`);
 
-function slice(arr, i, j, k) {
-    if(k==0){
-        throw new Error('slice:error slice(i,j,k) k不能是0')
+    //1、如果i或j是numpy负索引，则转成正常索引
+    i = i < 0 ? d + i : i;
+    j = j < 0 ? d + j : j;
+
+    //2、如果i或j超越了数组范围，将超越范围的值，进行正常化处理
+    if (k > 0) {
+        i = i <= 0 ? 0 : i;
+        j = j >= d ? d : j;
+    } else if (k < 0) {
+        i = i >= (d - 1) ? (d - 1) : i;
+        j = j <= -1 ? -1 : j;
     }
-    let n = arr.length;
-    if (typeof k != 'number') {
-        k = 1;
-    }
-    if (typeof i != 'number') {
-        i = k>0?0:n-1;
-    }
-    if (typeof j != 'number') {
-        j = k>0?n:-n-1;
-    }
 
-    
-    
+    //3、使用(j-i)/k 和 (j-i)%k进行计算
+    let q = Math.floor((j - i) / k);
+    let r = (j - i) % k;
+    let m = r == 0 ? (q + r) : (q + 1);
+    m = m <= 0 ? 0 : m;
 
+    console.log(`after d=${d} i=${i} j=${j} k=${k} q=${q} r=${r} m=${m}`);
+    console.log(`j-i==q*k+r ${j - i == q * k + r}`);
+    console.log(`i+(m-1)*k<j ${i + (m - 1) * k < j}`);
+    console.log(`i+(m-1)*k ${i + (m - 1) * k}`);
 
-
-
-
+    let indexArr = [];
     let index = i;
-    let newArr = [];
-    while (index >= 0 && index < arr.length) {
-        newArr.push(arr[index])
+    while (indexArr.length != m) {
+        indexArr.push(index)
         index = index + k;
     }
-    console.log('newArr：', newArr);
+    console.log('indexArr', indexArr);
 }
 
-// slice(arr,6,1,-1)
-// newArr： [ 6, 5, 4, 3, 2, 1, 0]
+// slice(10,1,7,2)
+// before d=10 i=1 j=7 k=2
+// after d=10 i=1 j=7 k=2 q=3 r=0 m=3
+// j-i==q*k+r true
+// i+(m-1)*k<j true
+// i+(m-1)*k 5
+// indexArr [ 1, 3, 5 ]
 
-// slice(arr,1,6,2)
-// newArr： [ 1, 3, 5 ]
+// slice(10,1,7,3)
+// before d=10 i=1 j=7 k=3
+// after d=10 i=1 j=7 k=3 q=2 r=0 m=2
+// j-i==q*k+r true
+// i+(m-1)*k<j true
+// i+(m-1)*k 4
+// indexArr [ 1, 4 ]
 
-// slice(arr,6,1,-2)
-// newArr： [ 6, 4, 2, 0 ]
+// slice(10,1,8,3)
+// before d=10 i=1 j=8 k=3
+// after d=10 i=1 j=8 k=3 q=2 r=1 m=3
+// j-i==q*k+r true
+// i+(m-1)*k<j true
+// i+(m-1)*k 7
+// indexArr [ 1, 4, 7 ]
 
-// slice(arr,6,1,-3)
-// newArr： [6, 3, 0]
+// slice(10,0,8,3)
+// before d=10 i=0 j=8 k=3
+// after d=10 i=0 j=8 k=3 q=2 r=2 m=3
+// j-i==q*k+r true
+// i+(m-1)*k<j true
+// i+(m-1)*k 6
+// indexArr [ 0, 3, 6 ]
+
+// slice(10,0,8,2)
+// before d=10 i=0 j=8 k=2
+// after d=10 i=0 j=8 k=2 q=4 r=0 m=4
+// j-i==q*k+r true
+// i+(m-1)*k<j true
+// i+(m-1)*k 6
+// indexArr [ 0, 2, 4, 6 ]
+
+//----------------------------------
+
+// 以下是 slice(i,j,k) 里k是负值时
+
+//----------------------------------
+
+// slice(10,7,1,-2)
+// before d=10 i=7 j=1 k=-2
+// after d=10 i=7 j=1 k=-2 q=3 r=0 m=3
+// j-i==q*k+r true
+// i+(m-1)*k<j false
+// i+(m-1)*k 3
+// indexArr [ 7, 5, 3 ]
+
+// slice(10,7,1,-3)
+// before d=10 i=7 j=1 k=-3
+// after d=10 i=7 j=1 k=-3 q=2 r=0 m=2
+// j-i==q*k+r true
+// i+(m-1)*k<j false
+// i+(m-1)*k 4
+// indexArr [ 7, 4 ]
+
+// slice(10,8,1,-3)
+// before d=10 i=8 j=1 k=-3
+// after d=10 i=8 j=1 k=-3 q=2 r=-1 m=3
+// j-i==q*k+r true
+// i+(m-1)*k<j false
+// i+(m-1)*k 2
+// indexArr [ 8, 5, 2 ]
+
+// slice(10,8,0,-3)
+// before d=10 i=8 j=0 k=-3
+// after d=10 i=8 j=0 k=-3 q=2 r=-2 m=3
+// j-i==q*k+r true
+// i+(m-1)*k<j false
+// i+(m-1)*k 2
+// indexArr [ 8, 5, 2 ]
+
+// slice(10,8,0,-2)
+// before d=10 i=8 j=0 k=-2
+// after d=10 i=8 j=0 k=-2 q=4 r=0 m=4
+// j-i==q*k+r true
+// i+(m-1)*k<j false
+// i+(m-1)*k 2
+// indexArr [ 8, 6, 4, 2 ]
+
+//----------------------------------
+
+// slice(10,8,-3,-2)
+// before d=10 i=8 j=-3 k=-2
+// after d=10 i=8 j=7 k=-2 q=0 r=-1 m=1
+// j-i==q*k+r true
+// i+(m-1)*k<j false
+// i+(m-1)*k 8
+// indexArr [ 8 ]
+
+// slice(10,-10-2,100,3)
+// before d=10 i=-12 j=100 k=3
+// after d=10 i=0 j=10 k=3 q=3 r=1 m=4
+// j-i==q*k+r true
+// i+(m-1)*k<j true
+// i+(m-1)*k 9
+// indexArr [ 0, 3, 6, 9 ]
+
+// slice(10,100,-10-2,-3)
+// before d=10 i=100 j=-12 k=-3
+// after d=10 i=9 j=-1 k=-3 q=3 r=-1 m=4
+// j-i==q*k+r true
+// i+(m-1)*k<j false
+// i+(m-1)*k 0
+// indexArr [ 9, 6, 3, 0 ]
+
+//----------------------------------
+
+// slice(10, 100, -10-2, 3)
+// indexArr []
+
+// slice(10, -10-2, 100, -3)
+// indexArr []
+
+// slice(10, 9, 9, -3)
+// indexArr []
+
+// slice(10, 10, 10, -3)
+// indexArr []
+
+// slice(10, 0, 0, -3)
+// indexArr []
+
+// slice(10, -11, -11, -3)
+// indexArr []
+
+
+slice(4, 4,-11,-1)
