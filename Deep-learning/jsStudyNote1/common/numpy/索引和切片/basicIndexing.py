a00116f98d3b98d668f66b4c61d70ab5ca16d93a
kangyupeng <kangyupeng@goatai.cn>
2023-12-14 18:44:18 +0800

    整理索引切片资料

--- Deep-learning/jsStudyNote1/common/numpy/索引和切片/basicIndexing.py
+++ Deep-learning/jsStudyNote1/common/numpy/索引和切片/basicIndexing.py
@@ -311,7 +311,7 @@ a[1,1:3:1,:]
 
 # 注意：以上情况没有考虑 切片slice(i,j,k) i和j是负值的情况，如果i和j是负值，i和j应该先从numpy负索引，转成正常索引，然后再进行操作
 # 注意：以上情况没有考虑 切片slice(i,j,k) k是负值的情况
-# 结果方法，请看 numpy_slice6.py 和 slice_test.js
+# 解决方法，请看 ‘关于切片slice/numpy_slice.py’ 和 ‘关于切片slice/slice_test.js’
 
 
 '''
