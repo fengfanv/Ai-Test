c2e94cb3bf32a6ae728c399450ecbfb87cb5dc2f
fengfanv <1431826670@qq.com>
2024-08-28 22:23:45 +0800

    新增numpy.max和numpy.argmax和toStr方法

--- Deep-learning/jsStudyNote1/common/numpy/extra.js
+++ Deep-learning/jsStudyNote1/common/numpy/extra.js
@@ -4,8 +4,14 @@ var reshape = Reshape.reshape;
 const Transpose = require('./transpose.js')
 var transpose = Transpose.transpose;
 
+const Axis = require('./axis.js')
+var get_axis = Axis.get_axis;
+
 const Main = require('./main.js')
 var arange = Main.arange;
+
+const printTest = require('./print_test.js')
+var toStr = printTest.toStr;
 
 function flatten(arr, order) {
     if (typeof order == 'undefined') {
@@ -24,3 +30,107 @@ function flatten(arr, order) {
     }
 }
 exports.flatten = flatten;
+
+function getMax(arr) {
+    let index = 0;
+    let item = arr[index];
+    for (let i = 0; i < arr.length; i++) {
+        if (arr[i].value > item.value) {
+            index = i;
+            item = arr[i];
+        }
+    }
+    return {
+        index,
+        item
+    }
+}
+
+function getMin(arr) {
+    let index = 0;
+    let item = arr[index];
+    for (let i = 0; i < arr.length; i++) {
+        if (arr[i].value < item.value) {
+            index = i;
+            item = arr[i];
+        }
+    }
+    return {
+        index,
+        item
+    }
+}
+
+function max(arr, axis) {
+    if (typeof arr == 'undefined') {
+        throw new Error('错误：arr不能为空')
+    }
+
+    if (Array.isArray(arr) || typeof arr == 'number') {
+        if (typeof arr == 'number') arr = [arr]
+    } else {
+        throw new Error('错误：arr数据类型不正确，sum仅支持Number Or Array')
+    }
+
+    if (typeof axis == 'undefined') {
+        let f_arr = flatten(arr);
+        let index = 0;
+        for (let i = 0; i < f_arr.length; i++) {
+            if (f_arr[i] > f_arr[index]) {
+                index = i;
+            }
+        }
+        return f_arr[index]
+    } else {
+        let axisInfo = get_axis(arr, axis)
+        let axisArr = axisInfo.axisArr2
+        for (let i = 0; i < axisArr.length; i++) {
+            axisArr[i] = getMax(axisArr[i]).item.value
+        }
+        let resultShape = axisInfo.resultShape
+        let resultData = resultShape.length == 0 ? Number(axisArr) : reshape(axisArr, resultShape)
+        return resultData
+    }
+}
+exports.max = max;
+
+function argmax(arr, axis) {
+    if (typeof arr == 'undefined') {
+        throw new Error('错误：arr不能为空')
+    }
+
+    if (Array.isArray(arr) || typeof arr == 'number') {
+        if (typeof arr == 'number') arr = [arr]
+    } else {
+        throw new Error('错误：arr数据类型不正确，sum仅支持Number Or Array')
+    }
+
+    if (typeof axis == 'undefined') {
+        let f_arr = flatten(arr);
+        let index = 0;
+        for (let i = 0; i < f_arr.length; i++) {
+            if (f_arr[i] > f_arr[index]) {
+                index = i;
+            }
+        }
+        return index
+    } else {
+        let axisInfo = get_axis(arr, axis)
+        let axisArr = axisInfo.axisArr2
+        for (let i = 0; i < axisArr.length; i++) {
+            axisArr[i] = getMax(axisArr[i]).index
+        }
+        let resultShape = axisInfo.resultShape
+        let resultData = resultShape.length == 0 ? Number(axisArr) : reshape(axisArr, resultShape)
+        return resultData
+    }
+}
+exports.argmax = argmax
+
+
+// var a = reshape(arange(1 * 2 * 3 * 4), [1, 2, 3, 4])
+// a[0][1][1][1] = 100
+
+// console.log(toStr(a))
+
+// console.log(toStr(argmax(a,0)))
--- Deep-learning/jsStudyNote1/common/numpy/print_test.js
+++ Deep-learning/jsStudyNote1/common/numpy/print_test.js
@@ -1,49 +1,49 @@
-var data = {
-    "code": 200,
-    "msg": "操作成功",
-    "data": {
-        "projectMonths": [
-            {
-                "month": "05",
-                "approach_count": undefined,
-                "retreat_count": null
-            },
-            {
-                "month": "06",
-                "approach_count": {},
-                "retreat_count": [2, { b: 2 }, 1]
-            },
-            {
-                "month": "07",
-                "approach_count": function (a, b) { return 'xxx' },
-                "retreat_count": 0
-            },
-            {
-                "month": "08",
-                "approach_count": [],
-                "retreat_count": [{ a: 1 }, 1]
-            },
-            {
-                "month": "09",
-                "approach_count": [1, 2, 3, 4, 5],
-                "retreat_count": [
-                    [0, 1, 2, 3],
-                    [4, 5, 6, 7],
-                    [8, 9, 10, 11]
-                ]
-            },
-            {
-                "month": "10",
-                "approach_count": [1, [[1, 2]]],
-                "retreat_count": 0
-            }
-        ],
-        "approachCount": 6,
-        "contractCount": 2,
-        "retreatCount": 0,
-        "customerCount": 2
-    }
-}
+// var data = {
+//     "code": 200,
+//     "msg": "操作成功",
+//     "data": {
+//         "projectMonths": [
+//             {
+//                 "month": "05",
+//                 "approach_count": undefined,
+//                 "retreat_count": null
+//             },
+//             {
+//                 "month": "06",
+//                 "approach_count": {},
+//                 "retreat_count": [2, { b: 2 }, 1]
+//             },
+//             {
+//                 "month": "07",
+//                 "approach_count": function (a, b) { return 'xxx' },
+//                 "retreat_count": 0
+//             },
+//             {
+//                 "month": "08",
+//                 "approach_count": [],
+//                 "retreat_count": [{ a: 1 }, 1]
+//             },
+//             {
+//                 "month": "09",
+//                 "approach_count": [1, 2, 3, 4, 5],
+//                 "retreat_count": [
+//                     [0, 1, 2, 3],
+//                     [4, 5, 6, 7],
+//                     [8, 9, 10, 11]
+//                 ]
+//             },
+//             {
+//                 "month": "10",
+//                 "approach_count": [1, [[1, 2]]],
+//                 "retreat_count": 0
+//             }
+//         ],
+//         "approachCount": 6,
+//         "contractCount": 2,
+//         "retreatCount": 0,
+//         "customerCount": 2
+//     }
+// }
 
 /**
  * 格式化数组或对象（用于解决JSON.stringify会把undefined和function忽略的问题）
@@ -54,6 +54,10 @@ var data = {
  * @returns String
  */
 function print(data, num, parentIsArr, isLast) {
+    if (Array.isArray(data) == false && Object.prototype.toString.call(data) != '[object Object]') {
+        return String(data);
+    }
+
     let space = 2;
     if (num == undefined) {
         num = 0;
@@ -164,6 +168,7 @@ function print(data, num, parentIsArr, isLast) {
     }
     return start + content + end;
 }
+exports.toStr = print;
 
 // console.log(data)
 // console.log(JSON.stringify(data, null, 2))
