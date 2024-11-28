9727de101310565181b5085372705617f530b19c
fengfanv <1431826670@qq.com>
2024-11-28 20:18:41 +0800

    新增numpy更多，一维和多维之间映射(关联)示例demo

--- Deep-learning/jsStudyNote1/common/numpy/single_multiple_relation.js
+++ Deep-learning/jsStudyNote1/common/numpy/single_multiple_relation.js
@@ -48,37 +48,144 @@ function computeMultiArr(arr, multiShape, getIndex = []) {
   return arr.slice(start, end)[0];
 }
 
+//根据上面的方法，chatGpt给出的，更直观，更有效率，的方法
+function computeMultiArrEfficient(arr, multiShape, getIndex = []) {
+  const numDimensions = multiShape.length;
+
+  // 计算每个维度的步长，步长是多维数组中每个维度后面所有维度的元素数目。
+  const strides = new Array(numDimensions);
+  strides[numDimensions - 1] = 1;  // 最后一维的步长为1
+  for (let i = numDimensions - 2; i >= 0; i--) {
+    strides[i] = strides[i + 1] * multiShape[i + 1];
+  }
+
+  // console.log('strides', strides)
+
+  // 计算索引在一维数组中的位置
+  let index = 0;
+  for (let i = 0; i < numDimensions; i++) {
+    index += getIndex[i] * strides[i];
+  }
+
+  // 返回一维数组中对应位置的元素
+  return arr[index];
+}
+
 // console.log(readArrByShape(a, aShape))
 
 // console.log(computeMultiArr(a, [2, 3, 4], [0, 0, 0]))
+// console.log(computeMultiArrEfficient(a, [2, 3, 4], [0, 0, 0]))
 // console.log(readArrByShape(a, aShape)[0][0][0])
 
 // console.log(computeMultiArr(a, [2, 3, 4], [0, 1, 0]))
+// console.log(computeMultiArrEfficient(a, [2, 3, 4], [0, 1, 0]))
 // console.log(readArrByShape(a, aShape)[0][1][0])
 
 // console.log(computeMultiArr(a, [2, 3, 4], [1, 1, 1]))
+// console.log(computeMultiArrEfficient(a, [2, 3, 4], [1, 1, 1]))
 // console.log(readArrByShape(a, aShape)[1][1][1])
 
 // console.log(computeMultiArr(a, [2, 3, 4], [1, 2, 1]))
+// console.log(computeMultiArrEfficient(a, [2, 3, 4], [1, 2, 1]))
 // console.log(readArrByShape(a, aShape)[1][2][1])
 
 // console.log(computeMultiArr(a, [2, 3, 4], [1, 2, 3]))
+// console.log(computeMultiArrEfficient(a, [2, 3, 4], [1, 2, 3]))
 // console.log(readArrByShape(a, aShape)[1][2][3])
 
 // console.log(computeMultiArr(a, [2, 3, 4], [2, 3, 4])) //返回undefined
+// console.log(computeMultiArrEfficient(a, [2, 3, 4], [2, 3, 4])) //返回undefined
 // console.log(readArrByShape(a, aShape)[2][3][4]) //报错
 
 // console.log(computeMultiArr(a, [2, 3, 4], [1, 0, 1]))
+// console.log(computeMultiArrEfficient(a, [2, 3, 4], [1, 0, 1]))
 // console.log(readArrByShape(a, aShape)[1][0][1])
 
 // console.log(computeMultiArr(a, [2, 3, 4], [1, 0, 0]))
+// console.log(computeMultiArrEfficient(a, [2, 3, 4], [1, 0, 0]))
 // console.log(readArrByShape(a, aShape)[1][0][0])
 
 // console.log(computeMultiArr(a, [2, 3, 4], [0, 1, 1]))
+// console.log(computeMultiArrEfficient(a, [2, 3, 4], [0, 1, 1]))
 // console.log(readArrByShape(a, aShape)[0][1][1])
 
 // console.log(computeMultiArr(a, [1, 2, 1, 3, 4, 1], [0, 1, 0, 1, 1, 0]));
+// console.log(computeMultiArrEfficient(a, [1, 2, 1, 3, 4, 1], [0, 1, 0, 1, 1, 0]));
 // console.log(readArrByShape(a, [1, 2, 1, 3, 4, 1])[0][1][0][1][1][0]);
 
 // console.log(computeMultiArr(a, [1, 1, 2, 1, 3, 4, 1], [0, 0, 1, 0, 1, 1, 0]));
+// console.log(computeMultiArrEfficient(a, [1, 1, 2, 1, 3, 4, 1], [0, 0, 1, 0, 1, 1, 0]));
 // console.log(readArrByShape(a, [1, 1, 2, 1, 3, 4, 1])[0][0][1][0][1][1][0]);
+
+
+//------------------------------------------------------------
+//------------------------------------------------------------
+//------------------------------------------------------------
+//------------------------------------------------------------
+
+//功能进化版
+function computeMultiArrEfficient_2(arr, multiShape, getIndex = []) {
+  const numDimensions = multiShape.length;
+
+  // 计算每个维度的步长，步长是多维数组中每个维度后面所有维度的元素数目。
+  const strides = new Array(numDimensions);
+  strides[numDimensions - 1] = 1;  // 最后一维的步长为1
+  for (let i = numDimensions - 2; i >= 0; i--) {
+    strides[i] = strides[i + 1] * multiShape[i + 1];
+  }
+
+  // console.log('strides', strides)
+
+  // 计算索引在一维数组中的位置
+  let index = 0;
+  for (let i = 0; i < getIndex.length; i++) {
+    index += getIndex[i] * strides[i];
+  }
+
+  let index_end = multiShape.slice(getIndex.length).reduce(function (prev, item, index, arr) {
+    return (prev *= item);
+  }, 1);
+
+  index_end = index + index_end
+
+  // console.log(index, index_end)
+  console.log("结果数据", arr.slice(index, index_end))
+  console.log("结果形状", multiShape.slice(getIndex.length))
+
+}
+
+// computeMultiArrEfficient_2(a, [2, 3, 4], [0, 0, 0])
+// console.log(readArrByShape(a, [2, 3, 4])[0][0][0])
+
+// computeMultiArrEfficient_2(a, [2, 3, 4], [0, 0])
+// console.log(readArrByShape(a, [2, 3, 4])[0][0])
+
+// computeMultiArrEfficient_2(a, [2, 3, 4], [0])
+// console.log(readArrByShape(a, [2, 3, 4])[0])
+
+// computeMultiArrEfficient_2(a, [2, 3, 4], [1])
+// console.log(readArrByShape(a, [2, 3, 4])[1])
+
+// computeMultiArrEfficient_2(a, [2, 3, 4], [1, 0])
+// console.log(readArrByShape(a, [2, 3, 4])[1][0])
+
+// computeMultiArrEfficient_2(a, [2, 3, 4], [1, 0, 2])
+// console.log(readArrByShape(a, [2, 3, 4])[1][0][2])
+
+// computeMultiArrEfficient_2(a, [2, 3, 4], [1, 2, 0])
+// console.log(readArrByShape(a, [2, 3, 4])[1][2][0])
+
+// computeMultiArrEfficient_2(a, [2, 3, 4], [1, 2])
+// console.log(readArrByShape(a, [2, 3, 4])[1][2])
+
+// computeMultiArrEfficient_2(a, [1, 2, 3, 1, 4, 1], [0, 1])
+// console.log(JSON.stringify(readArrByShape(a, [1, 2, 3, 1, 4, 1])[0][1]))
+
+// computeMultiArrEfficient_2(a, [1, 2, 3, 1, 4, 1], [])
+// console.log(JSON.stringify(readArrByShape(a, [1, 2, 3, 1, 4, 1])))
+
+// computeMultiArrEfficient_2(a, [1, 1, 2, 3, 1, 4, 1], [0, 0, 1, 2, 0, 3, 0])
+// console.log(JSON.stringify(readArrByShape(a, [1, 1, 2, 3, 1, 4, 1])[0][0][1][2][0][3][0]))
+
+// computeMultiArrEfficient_2(a, [1, 1, 2, 3, 1, 4, 1], [0, 0, 1, 2])
+// console.log(JSON.stringify(readArrByShape(a, [1, 1, 2, 3, 1, 4, 1])[0][0][1][2]))
