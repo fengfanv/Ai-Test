37ab40fe205be541c797e33c66138aea73a833d7
fengfanv <1431826670@qq.com>
2024-12-15 14:21:15 +0800

    二刷深度学习入门

--- Deep-learning/jsStudyNote1/demo1/index2.js
+++ Deep-learning/jsStudyNote1/demo1/index2.js
@@ -20,4 +20,38 @@ m.goodbye()
 
 const np = require('../common/numpy');
 
-console.log('np',np)
+// console.log('np', np)
+
+// let a = [[51, 55], [14, 19], [0, 4]]
+// let X = np.flatten(a)
+// console.log(np.indexing(X, [[0, 2, 4]]))
+// console.log(np.expr(X, '>', 15))
+// console.log(np.indexing(X, [np.expr(X, '>', 15)]))
+
+const matplotlib = require('../common/matplotlib')
+
+// var x = np.arange(0, 6, 0.1)
+// var y = np.sin(x)
+// var y2 = np.cos(x)
+// //折线图
+// matplotlib.chart.line([{
+//     label: 'sin',
+//     x: x,
+//     y: y
+// },
+// {
+//     label: 'cos',
+//     x: x,
+//     y: y2
+// }])
+
+// // matplotlib.image('./lena.png')
+// matplotlib.imread('./lena.png', (imageData) => {
+//     console.log(imageData)
+
+//     const { width, height, data } = imageData
+
+//     let image = np.reshape(data, [height, width, 4])
+
+//     matplotlib.imshow(image)
+// })
